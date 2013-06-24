823d401bddfeb84a3401b14692226e7cc81cdfc4
omar delarosa <thedelarosa@gmail.com>
2013-06-24 15:05:16 -0400

    added displaying of Task.all in the tasks/new page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
 class TasksController < ApplicationController
 	def new
 		@task = Task.new
+		@tasks = Task.all
 	end
 
 	def create
-		@task = Task.new(desc: params['desc'])
-		@task.save!
-		render nothing: true
+		task = Task.new(desc: params['desc'])
+		task.save!
+		render text: task.desc
+	end
+
+	def index
+		@tasks = Task.all
 	end
 end
